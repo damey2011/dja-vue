64001b2c49d09645e3757bda96219f693bba6ba7
Damilola Adeyemi <neefemee@gmail.com>
2020-03-25 04:25:20 +0100

    initial commit

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,14 @@
+const ASSET_DIR = 'static';
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+
 module.exports = {
-    assetsDir: 'static'
+    assetsDir: ASSET_DIR,
+    configureWebpack: {
+    plugins: [
+      new FaviconsWebpackPlugin({
+          logo: './src/assets/favicon.png',
+          prefix: ASSET_DIR + '/favicons'
+      })
+    ]
+  }
 };
