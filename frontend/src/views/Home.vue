f091ce82457b61eaedece70216a007d6da765250
Damilola Adeyemi <neefemee@gmail.com>
2020-03-28 20:26:23 +0100

    remove purgecss as purge css doesn't find all required classes in buefy

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="Vue logo" class="is-full" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Django + Vue.js App" />
+    <section>
+
+        <div class="buttons">
+            <b-button type="is-primary">Primary</b-button>
+
+            <b-button type="is-success">Success</b-button>
+
+            <b-button type="is-danger">Danger</b-button>
+
+            <b-button type="is-warning">Warning</b-button>
+
+            <b-button type="is-info">Info</b-button>
+
+            <b-button type="is-link">Link</b-button>
+
+            <b-button type="is-light">Light</b-button>
+
+            <b-button type="is-dark">Dark</b-button>
+
+            <b-button type="is-text">Text</b-button>
+        </div>
+
+        <div class="buttons">
+            <b-button disabled>Disabled</b-button>
+
+            <b-button loading>Loading</b-button>
+
+            <b-button rounded>Rounded</b-button>
+        </div>
+
+        <div class="buttons">
+            <b-button type="is-primary" outlined>Outlined</b-button>
+            <b-button type="is-success" outlined>Outlined</b-button>
+            <b-button type="is-danger" outlined>Outlined</b-button>
+            <b-button type="is-warning" outlined>Outlined</b-button>
+        </div>
+
+        <div class="buttons">
+            <b-button type="is-primary" expanded>Expanded</b-button>
+        </div>
+
+        <div class="notification is-primary">
+            <b-button type="is-primary"
+                inverted>
+                Inverted
+            </b-button>
+
+            <b-button type="is-primary"
+                inverted
+                outlined>
+                Invert Outlined
+            </b-button>
+        </div>
+
+    </section>
   </div>
 </template>
 
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,14 +1,14 @@
 const ASSET_DIR = 'static';
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
     assetsDir: ASSET_DIR,
     configureWebpack: {
-    plugins: [
-      new FaviconsWebpackPlugin({
-          logo: './src/assets/favicon.png',
-          prefix: ASSET_DIR + '/favicons'
-      })
-    ]
-  }
+        plugins: [
+            new FaviconsWebpackPlugin({
+                logo: './src/assets/favicon.png',
+                prefix: ASSET_DIR + '/favicons'
+            })
+        ]
+    }
 };
